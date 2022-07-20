0152dadf252d96896ed812bd194305d3cf1d498c
Alberto Beltran <albertobeltran@protonmail.com>
2022-07-20 05:51:06 -0500

    implemented getOwnedPokemons function

--- contracts/PokemonFactory.sol
+++ contracts/PokemonFactory.sol
@@ -12,16 +12,28 @@ contract PokemonFactory {
     Pokemon[] private pokemons;
 
     mapping (uint => address) public pokemonToOwner;
-    mapping (address => uint) ownerPokemonCount;
+    mapping (address => uint) public ownerPokemonCount;
+    mapping (address => mapping(uint => Pokemon)) ownedPokemons;
 
-     function createPokemon (string memory _name, uint _id) public {
-        pokemons.push(Pokemon(_id, _name));
-        pokemonToOwner[_id] = msg.sender;
-        ownerPokemonCount[msg.sender]++;
+    function createPokemon (string memory _name) public {
+      uint id = pokemons.length;
+      pokemons.push(Pokemon(id, _name));
+      pokemonToOwner[id] = msg.sender;
+      ownerPokemonCount[msg.sender]++;
+      ownedPokemons[msg.sender][ownerPokemonCount[msg.sender]] = pokemons[id];
     }
 
     function getAllPokemons() public view returns (Pokemon[] memory) {
       return pokemons;
     }
 
+    function getOwnedPokemons() public view returns (Pokemon[] memory) {
+      uint counter = ownerPokemonCount[msg.sender];
+
+      Pokemon[] memory ownedPokemonsArr = new Pokemon[](counter);
+      for(uint8 i=0; i<counter; i++) {
+        ownedPokemonsArr[i] = ownedPokemons[msg.sender][i+1];
+      }
+      return ownedPokemonsArr;
+    }
 }
