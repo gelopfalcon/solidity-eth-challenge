5697e0ddfd0373268259c063fcdebd9887793b27
R-Livise <35082874+R-Livise@users.noreply.github.com>
2022-07-20 17:56:04 -0500

    feat: [UPDATE] Reto 2

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -17,6 +17,8 @@ contract PokemonFactory {
     event eventNewPokemon( uint _id, string _name );
 
     function createPokemon ( uint _id , string memory _name ) public {
+        require(_id > 0, "_id must be greater than 0.");
+        require(bytes(_name).length > 2, "_name must be greater than 2 characters.");
         pokemons.push(Pokemon(_id, _name));
         pokemonToOwner[_id] = msg.sender;
         ownerPokemonCount[msg.sender]++;
