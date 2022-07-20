25f2a61b08fe18861e66956f0ab6a6e3c3b08ba0
Alberto Beltran <albertobeltran@protonmail.com>
2022-07-20 05:34:08 -0500

    initial commit - set up hardhat

--- contracts/PokemonFactory.sol
+++ contracts/PokemonFactory.sol
@@ -24,12 +24,4 @@ contract PokemonFactory {
       return pokemons;
     }
 
-
-    function getResult() public pure returns(uint product, uint sum){
-      uint a = 1; 
-      uint b = 2;
-      product = a * b;
-      sum = a + b; 
-   }
-
 }
---
+++ scripts/deploy.js
@@ -0,0 +1,35 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  [creator] =  await ethers.getSigners();
+
+  const PokemonFactory = await hre.ethers.getContractFactory("PokemonFactory");
+  const Pokemon = await PokemonFactory.deploy();
+
+  await Pokemon.deployed();
+
+  console.log("PokemonFactory deployed to:", Pokemon.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
