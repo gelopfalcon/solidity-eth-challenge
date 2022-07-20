a03c7b07bef5ff0678e16084c8370b46862e0312
Armando Rivera <rivera.armando997@gmail.com>
2022-07-20 18:49:36 -0500

    ch1 completed

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -3,33 +3,34 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract PokemonFactory {
+    struct Pokemon {
+        uint id;
+        string name;
+    }
 
-  struct Pokemon {
-    uint id;
-    string name;
-  }
+    event eventNewPokemon(Pokemon pokemon);
 
     Pokemon[] private pokemons;
 
-    mapping (uint => address) public pokemonToOwner;
-    mapping (address => uint) ownerPokemonCount;
+    mapping(uint => address) public pokemonToOwner;
+    mapping(address => uint) ownerPokemonCount;
 
-     function createPokemon (string memory _name, uint _id) public {
-        pokemons.push(Pokemon(_id, _name));
+    function createPokemon(string memory _name, uint _id) public {
+        Pokemon memory pokemon = Pokemon(_id, _name);
+        pokemons.push(pokemon);
         pokemonToOwner[_id] = msg.sender;
         ownerPokemonCount[msg.sender]++;
+        emit eventNewPokemon(pokemon);
     }
 
     function getAllPokemons() public view returns (Pokemon[] memory) {
-      return pokemons;
+        return pokemons;
     }
 
-
-    function getResult() public pure returns(uint product, uint sum){
-      uint a = 1; 
-      uint b = 2;
-      product = a * b;
-      sum = a + b; 
-   }
-
+    function getResult() public pure returns (uint product, uint sum) {
+        uint a = 1;
+        uint b = 2;
+        product = a * b;
+        sum = a + b;
+    }
 }
