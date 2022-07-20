d5e384261c678648c91f66b28893f5aadf53fa3b
Alberto Beltran <albertobeltran@protonmail.com>
2022-07-20 06:22:19 -0500

    #2 created eventNewPokemon

--- contracts/PokemonFactory.sol
+++ contracts/PokemonFactory.sol
@@ -3,37 +3,39 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract PokemonFactory {
+  event eventNewPokemon(uint256 id, string name);
 
   struct Pokemon {
-    uint id;
+    uint256 id;
     string name;
   }
 
-    Pokemon[] private pokemons;
+  Pokemon[] private pokemons;
 
-    mapping (uint => address) public pokemonToOwner;
-    mapping (address => uint) public ownerPokemonCount;
-    mapping (address => mapping(uint => Pokemon)) ownedPokemons;
+  mapping(uint256 => address) public pokemonToOwner;
+  mapping(address => uint256) public ownerPokemonCount;
+  mapping(address => mapping(uint256 => Pokemon)) ownedPokemons;
 
-    function createPokemon (string memory _name) public {
-      uint id = pokemons.length;
-      pokemons.push(Pokemon(id, _name));
-      pokemonToOwner[id] = msg.sender;
-      ownerPokemonCount[msg.sender]++;
-      ownedPokemons[msg.sender][ownerPokemonCount[msg.sender]] = pokemons[id];
+  function createPokemon(string memory _name) public {
+    uint256 id = pokemons.length;
+    pokemons.push(Pokemon(id, _name));
+    pokemonToOwner[id] = msg.sender;
+    ownerPokemonCount[msg.sender]++;
+    ownedPokemons[msg.sender][ownerPokemonCount[msg.sender]] = pokemons[id];
+    emit eventNewPokemon(id, _name);
+  }
+
+  function getAllPokemons() public view returns (Pokemon[] memory) {
+    return pokemons;
+  }
+
+  function getOwnedPokemons() public view returns (Pokemon[] memory) {
+    uint256 counter = ownerPokemonCount[msg.sender];
+
+    Pokemon[] memory ownedPokemonsArr = new Pokemon[](counter);
+    for (uint8 i = 0; i < counter; i++) {
+        ownedPokemonsArr[i] = ownedPokemons[msg.sender][i + 1];
     }
-
-    function getAllPokemons() public view returns (Pokemon[] memory) {
-      return pokemons;
-    }
-
-    function getOwnedPokemons() public view returns (Pokemon[] memory) {
-      uint counter = ownerPokemonCount[msg.sender];
-
-      Pokemon[] memory ownedPokemonsArr = new Pokemon[](counter);
-      for(uint8 i=0; i<counter; i++) {
-        ownedPokemonsArr[i] = ownedPokemons[msg.sender][i+1];
-      }
-      return ownedPokemonsArr;
-    }
+    return ownedPokemonsArr;
+  }
 }
