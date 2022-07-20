b8865a10efee544295df863c0e993d45bd4ca651
Aldanam88 <86798196+Aldanam88@users.noreply.github.com>
2022-07-20 14:17:34 -0300

    Retos 1 y 2

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -4,6 +4,9 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract PokemonFactory {
 
+  //Reto #1 declaración del evento
+  event eventNewPokemon (uint id, string name);
+
   struct Pokemon {
     uint id;
     string name;
@@ -15,9 +18,14 @@ contract PokemonFactory {
     mapping (address => uint) ownerPokemonCount;
 
      function createPokemon (string memory _name, uint _id) public {
+       //Reto #2 declaración de las condiciones y mensajes para el usuario
+       require(_id > 0,"El id no puede ser igual a 0");
+       require(bytes(_name).length > 2,"El nombre debe tener mas de 2 caracteres");
         pokemons.push(Pokemon(_id, _name));
         pokemonToOwner[_id] = msg.sender;
         ownerPokemonCount[msg.sender]++;
+        //Reto #1 emitir evento
+        emit eventNewPokemon(_id, _name);
     }
 
     function getAllPokemons() public view returns (Pokemon[] memory) {
