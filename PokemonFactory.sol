25d28ccf43ab9fd6c70a5e10823e3a40a9e31695
Armando Rivera <rivera.armando997@gmail.com>
2022-07-21 16:04:37 -0500

    Challenge 3

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -4,9 +4,14 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract PokemonFactory {
 
+    struct Ability {
+      string name;
+      string description;
+    }
+
     struct Pokemon {
-        uint id;
-        string name;
+      uint id;
+      string name;
     }
 
     event eventNewPokemon(
@@ -14,6 +19,8 @@ contract PokemonFactory {
     );
 
     Pokemon[] private pokemons;
+    Ability[] private abilites;
+    address author;
 
     modifier idMajor1(uint _id) {
       require(_id > 0, "You cannot add negative numbers nither zero id");
@@ -25,14 +32,27 @@ contract PokemonFactory {
       _;
     }
 
+    modifier isAuthor() {
+        require(msg.sender == author, "You cannot add abilites because you are not the author");
+        _;
+    }
+
     mapping (uint => address) public pokemonToOwner;
     mapping (address => uint) ownerPokemonCount;
+    mapping (uint => Pokemon) idToPokemon;
+    mapping (string => Ability) nameToAbility;
+    mapping (uint => Ability[]) pokemonToAbility;
 
-     function createPokemon (string memory _name, uint _id) public idMajor1(_id) validName(_name) {
+    constructor() {
+      author = msg.sender;
+    }
+
+    function createPokemon (string memory _name, uint _id) public idMajor1(_id) validName(_name) {
         Pokemon memory pokemon = Pokemon(_id, _name);
         pokemons.push(pokemon);
         pokemonToOwner[_id] = msg.sender;
         ownerPokemonCount[msg.sender]++;
+        idToPokemon[_id] = pokemon;
         emit eventNewPokemon(pokemon);
     }
 
@@ -40,6 +60,25 @@ contract PokemonFactory {
       return pokemons;
     }
 
+    function addAbilityToPokemon(uint _pokemonId, string memory _abiliteName) public {
+      Pokemon memory pokemon = idToPokemon[_pokemonId];
+      pokemonToAbility[pokemon.id].push( nameToAbility[_abiliteName]);
+    }
+
+
+    function createAbility(string memory _name, string memory _description) public isAuthor {
+      Ability memory ability = Ability(_name, _description);
+      abilites.push(ability);
+      nameToAbility[_name] = ability;
+    }
+
+    function getAllAbilites() public view returns (Ability[] memory) {
+      return abilites;
+    }
+
+    function getPokemonAbilities(uint _id) public view returns (Ability[] memory) {
+      return pokemonToAbility[_id];
+    }
 
     function getResult() public pure returns(uint product, uint sum){
       uint a = 1; 
