bf8fc824173b837909810e4475be04082ba20250
Diegoabustos <you@example.com>
2022-07-20 19:19:40 -0500

    feat: 🎸 challenge thre add abilities pokemon

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -7,6 +7,12 @@ contract PokemonFactory {
   struct Pokemon {
     uint id;
     string name;
+    Ability[] abilities;
+  }
+
+  struct Ability {
+    string name;
+    string description;
   }
 
     Pokemon[] private pokemons;
@@ -14,15 +20,27 @@ contract PokemonFactory {
     mapping (uint => address) public pokemonToOwner;
     mapping (address => uint) ownerPokemonCount;
 
-    event eventNewPokemon(uint _id, string _name);
+    event eventNewPokemon(Pokemon _pokemon);
 
-    function createPokemon (string memory _name, uint _id) public {
+    function createPokemon (string memory _name, uint _id, string[] memory _abilitiesName, string[] memory _abilitiesDescription) public {
         require(_id > 0, "El id ingresado debe ser mayor a 0");
         require(bytes(_name).length >= 2, "El nombre del Pokemon debe tener mas de dos careacteres");
-        pokemons.push(Pokemon(_id, _name));
-        emit eventNewPokemon(_id, _name);
+        uint index=pokemons.length;
+        pokemons.push();
+        pokemons[index].name=_name;
+        pokemons[index].id=_id;
+        addAbilitiesPokemon(index, _abilitiesName, _abilitiesDescription);
+        emit eventNewPokemon(pokemons[index]);
         pokemonToOwner[_id] = msg.sender;
         ownerPokemonCount[msg.sender]++;
+    }
+
+    function addAbilitiesPokemon(uint _index, string[] memory _abilitiesName, string[] memory _abilitiesDescription) private {
+      for(uint i=0; i < _abilitiesName.length; i++) {
+        pokemons[_index].abilities.push(
+          Ability(_abilitiesName[i], _abilitiesDescription[i])
+        );
+      }
     }
 
     function getAllPokemons() public view returns (Pokemon[] memory) {
