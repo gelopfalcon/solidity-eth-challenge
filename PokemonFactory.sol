703c415abb9e9f188457d4ccd2e7b03589d17db6
yfsanchez <yfsanchez@gmail.com>
2022-07-23 19:16:46 -0300

    Reto 2 actualizar cantidad de carateres en el nombre

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -18,7 +18,7 @@ contract PokemonFactory {
 
      function createPokemon (string memory _name, uint _id) public {
         require(_id > 0, "El id tiene que ser mayor a zero.");
-        require(bytes(_name).length > 1, "El nombre  debe tener mas de 2 carateres.");
+        require(bytes(_name).length > 2, "El nombre  debe tener mas de 2 carateres.");
         pokemons.push(Pokemon(_id, _name));
         pokemonToOwner[_id] = msg.sender;
         ownerPokemonCount[msg.sender]++;
