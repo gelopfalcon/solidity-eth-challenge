f6ea55d499889a17147779d3d2e621f986f2250b
yfsanchez <yfsanchez@gmail.com>
2022-07-24 00:43:50 -0300

    Reto #3

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -4,15 +4,33 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract PokemonFactory {
 
+  struct Ability {
+      string name;
+      string description;
+  }
   struct Pokemon {
     uint id;
     string name;
   }
 
+  struct PokemonType {
+      uint8 id;
+      string name;
+  }
+
     Pokemon[] private pokemons;
+    Ability[] private abilities;
+    PokemonType[] private pokemonTypes;
 
     mapping (uint => address) public pokemonToOwner;
     mapping (address => uint) ownerPokemonCount;
+
+    mapping (uint => Pokemon) idToPokemon;
+
+    mapping (string => Ability) nameToAbility;
+    mapping (uint => Ability[]) pokemonIdToAbility;
+
+    mapping (uint => PokemonType) idToPokemonTypes;
 
     event eventNewPokemon(Pokemon);
 
@@ -22,12 +40,30 @@ contract PokemonFactory {
         pokemons.push(Pokemon(_id, _name));
         pokemonToOwner[_id] = msg.sender;
         ownerPokemonCount[msg.sender]++;
+        idToPokemon[_id] = Pokemon(_id, _name);
         emit eventNewPokemon(Pokemon(_id, _name));
+    }
+    function createAbility (string memory _name, string memory _description) public {
+        require(bytes(_name).length > 1, "El nombre de la habilidad con al menos doscarateres.");
+        require(bytes(_description).length > 1, "Esfuerzo con la descripcion.");
+        abilities.push(Ability(_name,_description));
+        nameToAbility[_name] = Ability(_name,_description);
+
     }
 
     function getAllPokemons() public view returns (Pokemon[] memory) {
       return pokemons;
     }
+
+    function getAllAbilities() public view returns (Ability[] memory) {
+      return abilities;
+    }
+
+    function getAbilitiesFromPokemon(uint pokemonid) public view returns (Ability[] memory) {
+      return pokemonIdToAbility[pokemonid];
+    }
+
+
 
 
     function getResult() public pure returns(uint product, uint sum){
@@ -36,5 +72,9 @@ contract PokemonFactory {
       product = a * b;
       sum = a + b; 
    }
+
+   function addAbilityToPokemon(uint _pokemonId, string memory _abiliteName) public {
+      pokemonIdToAbility[idToPokemon[_pokemonId].id].push( nameToAbility[_abiliteName]);
+    }
 
 }
