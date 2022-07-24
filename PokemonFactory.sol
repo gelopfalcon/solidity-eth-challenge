5cebe11c21260fdc89eba1de8a74030cea656ade
rennypetit <rennypetit01@gmail.com>
2022-07-24 18:50:48 -0500

    add: third challenge completed

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -5,32 +5,56 @@ pragma solidity >=0.7.0 <0.9.0;
 contract PokemonFactory {
 
   struct Pokemon {
-    uint id;
+    uint8 id;
     string name;
   }
 
+  struct Ability {
+    string name; 
+    string description;
+  }
+
   Pokemon[] private pokemons;
+  Ability[] private abilities;
 
   mapping (uint => address) public pokemonToOwner;
   mapping (address => uint) ownerPokemonCount;
+  mapping (uint8 => Ability[]) public pokemonToAbility;
 
   event eventNewPokemon (string);
 
-  modifier createValidation(string memory _name , uint _id ) {
+  modifier createValidation(uint8 _id, string memory _name ) {
     require(_id > 0, "id no debe estar vacio y tiene que ser mayor a 0");
     require(bytes(_name).length > 2, "El nombre debe ser mayor de 2 caracteres");
     _; 
   }
 
-  function createPokemon (string memory _name, uint _id) public createValidation(_name, _id) {
+  modifier createValidationAbility(uint8 _id, string memory _name, string memory _description ) {
+    require(_id > 0, "id no debe estar vacio y tiene que ser mayor a 0");
+    require(bytes(_name).length > 2, "El nombre debe ser mayor de 2 caracteres");
+    require(bytes(_description).length > 2, "La descripcion debe ser mayor de 2 caracteres");
+
+    _; 
+  }
+
+  function createPokemon (uint8 _id, string memory _name) public createValidation(_id, _name) {
     pokemons.push(Pokemon(_id, _name));
     pokemonToOwner[_id] = msg.sender;
     ownerPokemonCount[msg.sender]++;
     emit eventNewPokemon( _name);
   }
 
+  function createAbility (uint8 _id, string memory _name, string memory _description) public createValidationAbility(_id, _name, _description) {
+    pokemonToAbility[_id].push(Ability(_name, _description));
+  }
+
+
   function getAllPokemons() public view returns (Pokemon[] memory) {
-    return pokemons;
+      return pokemons;
+    }
+
+  function getAllHabilities(uint8 _id) public view returns (Ability[] memory) {
+    return pokemonToAbility[_id];
   }
 
   function getResult() public pure returns(uint product, uint sum){
