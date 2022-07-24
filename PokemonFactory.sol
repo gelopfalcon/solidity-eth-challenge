edd704d45db007987c9af1fe22db156e03c7e73d
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-07-24 16:31:01 +0200

    challenge completed

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -3,33 +3,70 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract PokemonFactory {
-
-  struct Pokemon {
-    uint id;
-    string name;
-  }
+    struct Pokemon {
+        uint id;
+        string name;
+        string[] habilitiesName;
+        string[] pokemonTypes;
+        string[] weakness;
+    }
 
     Pokemon[] private pokemons;
 
-    mapping (uint => address) public pokemonToOwner;
-    mapping (address => uint) ownerPokemonCount;
+    //Events:
 
-     function createPokemon (string memory _name, uint _id) public {
-        pokemons.push(Pokemon(_id, _name));
+    event eventNewPokemon(
+        uint id,
+        string name,
+        string[] habilitiesName,
+        string[] pokemonTypes,
+        string[] weakness
+    );
+
+    mapping(uint => address) public pokemonToOwner;
+    mapping(address => uint) ownerPokemonCount;
+    mapping(string => string) habilitiesDescription;
+
+    //INSERT Functions:
+
+    function createPokemon(string memory _name, uint _id) public {
+        require(_id > 0, "The id should be greater than 0");
+        require(
+            keccak256(abi.encodePacked(_name)) !=
+                keccak256(abi.encodePacked("   ")),
+            "The name can not be a blank space"
+        );
+        require(
+            bytes(_name).length > 2,
+            "The name length should be greater than 2"
+        );
+
+        pokemons.push(Pokemon(_id, _name, _habilitiesName, _pokemonTypes, _weakness));
+
         pokemonToOwner[_id] = msg.sender;
         ownerPokemonCount[msg.sender]++;
+
+        emit eventNewPokemon(_id, _name, _habilitiesName, _pokemonTypes, _weakness);
     }
+
+    function insertDescription (string memory _name, string memory _habilityDescription) public {
+      habilitiesDescription[_name] = _habilityDescription;
+    }
+
+    //GET Functions:
 
     function getAllPokemons() public view returns (Pokemon[] memory) {
-      return pokemons;
+        return pokemons;
     }
 
+    function getHabilities(string memory _name) public view returns(string memory, string memory) {
+      return(_name, habilitiesDescription[_name]);
+    }
 
-    function getResult() public pure returns(uint product, uint sum){
-      uint a = 1; 
-      uint b = 2;
-      product = a * b;
-      sum = a + b; 
-   }
-
+    /* function getResult() public pure returns (uint product, uint sum) {
+        uint a = 1;
+        uint b = 2;
+        product = a * b;
+        sum = a + b;
+    } */
 }
