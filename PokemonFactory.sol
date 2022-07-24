370afc2e08c09bc6a84a436c226813b6926b0177
flizyx <nicolizarazo19@gmail.com>
2022-07-23 22:02:34 -0500

    [FEAT] added pokemonToHability, tested get Functions
    added mapping to pokemon´s habilities and require validations of users and inputs. multiple pokemons per address and multiple habilities per pokemon

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -8,28 +8,68 @@ contract PokemonFactory {
     uint id;
     string name;
   }
-
+  struct Hability {
+    string name;
+    string description;
+  }
     Pokemon[] private pokemons;
+    Hability[] private habilities;
 
     mapping (uint => address) public pokemonToOwner;
     mapping (address => uint) ownerPokemonCount;
+    mapping (uint => Hability[]) pokemonToHabilty;
+    mapping (uint => uint) pokemonHabilityCount;
+
+    event eventNewPokemon(Pokemon _pokemon);
+
 
      function createPokemon (string memory _name, uint _id) public {
+        require(_id > 0, "El id debe ser mayor a cero");
+        require(bytes(_name).length > 2 , "Corregir nombre de pokemon");
+
+
         pokemons.push(Pokemon(_id, _name));
         pokemonToOwner[_id] = msg.sender;
+
         ownerPokemonCount[msg.sender]++;
+
+        emit eventNewPokemon(Pokemon(_id, _name));
+    }
+    function evolvePokemon(uint256 _pokemonId, string memory _name,string memory _description) public {
+      require(_pokemonId > 0, "El id debe ser mayor a cero");
+      require(pokemonToOwner[_pokemonId] == msg.sender,"No tienes este pokemon");
+  
+
+      habilities.push(Hability(_name, _description));
+      
+      pokemonToHabilty[_pokemonId].push(Hability(_name, _description));
+      // pokemonToHabilty[_pokemonId]=Hability(_name, _description,_pokemonId);
+
+      pokemonHabilityCount[_pokemonId]++;
+
+    }
+    function getHabilityById(uint256 _pokemonId) public view returns (Hability[] memory) {
+      return pokemonToHabilty[_pokemonId];
+    }
+    
+    function getAllHabilities() public view returns (Hability[] memory) {
+      return habilities;
     }
 
     function getAllPokemons() public view returns (Pokemon[] memory) {
       return pokemons;
     }
-
-
-    function getResult() public pure returns(uint product, uint sum){
-      uint a = 1; 
-      uint b = 2;
-      product = a * b;
-      sum = a + b; 
-   }
+    function getPokemonsCount() public view returns (uint) {
+      return ownerPokemonCount[msg.sender];
+    }
+    function getHabilitiesCount(uint256 _pokemonId) public view returns (uint) {
+      return pokemonHabilityCount[_pokemonId];
+    }
+  //   function getResult() public pure returns(uint product, uint sum){
+  //     uint a = 1; 
+  //     uint b = 2;
+  //     product = a * b;
+  //     sum = a + b; 
+  //  }
 
 }
