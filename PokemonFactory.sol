5c4eb1e1b858aa19a4c003072ac40c73241ad4a3
Panchop <folivares@arenasoft.cl>
2022-07-25 23:32:21 +0100

    Reto 1

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -9,27 +9,33 @@ contract PokemonFactory {
     string name;
   }
 
-    Pokemon[] private pokemons;
+  event CreatePokemon(
+    address creator,
+    Pokemon newPokemon
+  );
 
-    mapping (uint => address) public pokemonToOwner;
-    mapping (address => uint) ownerPokemonCount;
+  Pokemon[] private pokemons;
 
-     function createPokemon (string memory _name, uint _id) public {
-        pokemons.push(Pokemon(_id, _name));
-        pokemonToOwner[_id] = msg.sender;
-        ownerPokemonCount[msg.sender]++;
-    }
+  mapping (uint => address) public pokemonToOwner;
+  mapping (address => uint) ownerPokemonCount;
 
-    function getAllPokemons() public view returns (Pokemon[] memory) {
-      return pokemons;
-    }
+    function createPokemon (string memory _name, uint _id) public {
+      pokemons.push(Pokemon(_id, _name));
+      pokemonToOwner[_id] = msg.sender;
+      ownerPokemonCount[msg.sender]++;
+      emit CreatePokemon(msg.sender, Pokemon(_id, _name));
+  }
+
+  function getAllPokemons() public view returns (Pokemon[] memory) {
+    return pokemons;
+  }
 
 
-    function getResult() public pure returns(uint product, uint sum){
-      uint a = 1; 
-      uint b = 2;
-      product = a * b;
-      sum = a + b; 
-   }
+  function getResult() public pure returns(uint product, uint sum){
+    uint a = 1; 
+    uint b = 2;
+    product = a * b;
+    sum = a + b; 
+  }
 
 }
