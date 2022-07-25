1e3e82ac55f14e23c9a4fcb3f47572e6b9d0f94c
rennypetit <rennypetit01@gmail.com>
2022-07-25 00:09:58 -0500

    add:fourth challenge completed

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -14,14 +14,26 @@ contract PokemonFactory {
     string description;
   }
 
+  struct TypeAndWeakness {
+    string name;
+  }
+
   Pokemon[] private pokemons;
   Ability[] private abilities;
+  TypeAndWeakness[] private types;
+  TypeAndWeakness[] private weaknesses;
+
 
   mapping (uint => address) public pokemonToOwner;
   mapping (address => uint) ownerPokemonCount;
-  mapping (uint8 => Ability[]) public pokemonToAbility;
+  mapping (uint8 => Ability[]) private pokemonToAbility;
+  mapping (uint8 => TypeAndWeakness[]) private pokemonToType;
+  mapping (uint8 => TypeAndWeakness[]) private pokemonToWeakness;
+
 
   event eventNewPokemon (string);
+  event eventNewOthers (string,string);
+
 
   modifier createValidation(uint8 _id, string memory _name ) {
     require(_id > 0, "id no debe estar vacio y tiene que ser mayor a 0");
@@ -46,6 +58,19 @@ contract PokemonFactory {
 
   function createAbility (uint8 _id, string memory _name, string memory _description) public createValidationAbility(_id, _name, _description) {
     pokemonToAbility[_id].push(Ability(_name, _description));
+    emit eventNewOthers("Ability created", _name);
+  }
+
+  function createType (uint8 _id, string memory _name) public createValidation(_id, _name) {
+    pokemonToType[_id].push(TypeAndWeakness(_name));
+    emit eventNewOthers("Type created", _name);
+
+  }
+
+  function createWeakness (uint8 _id, string memory _name) public createValidation(_id, _name) {
+    pokemonToWeakness[_id].push(TypeAndWeakness(_name));
+    emit eventNewOthers("Weakness created", _name);
+
   }
 
 
@@ -53,8 +78,15 @@ contract PokemonFactory {
       return pokemons;
     }
 
-  function getAllHabilities(uint8 _id) public view returns (Ability[] memory) {
+  function getAllAbilities(uint8 _id) public view returns (Ability[] memory) {
     return pokemonToAbility[_id];
+  }
+
+  function getAllTypes(uint8 _id) public view returns (TypeAndWeakness[] memory) {
+    return pokemonToType[_id];
+  }
+  function getAllWeaknesses(uint8 _id) public view returns (TypeAndWeakness[] memory) {
+    return pokemonToWeakness[_id];
   }
 
   function getResult() public pure returns(uint product, uint sum){
