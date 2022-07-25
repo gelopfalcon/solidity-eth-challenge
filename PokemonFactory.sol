9a80d19e1a2397d481baf597e67ddac1cd67cf82
jho3r <jhoer.perez@gmail.com>
2022-07-24 23:03:51 -0500

    Solving challenge 1: events

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -9,27 +9,33 @@ contract PokemonFactory {
     string name;
   }
 
-    Pokemon[] private pokemons;
+  Pokemon[] private pokemons;
 
-    mapping (uint => address) public pokemonToOwner;
-    mapping (address => uint) ownerPokemonCount;
+  mapping (uint => address) public pokemonToOwner;
+  mapping (address => uint) ownerPokemonCount;
 
-     function createPokemon (string memory _name, uint _id) public {
-        pokemons.push(Pokemon(_id, _name));
-        pokemonToOwner[_id] = msg.sender;
-        ownerPokemonCount[msg.sender]++;
-    }
+  event eventNewPokemon(
+    Pokemon pokemon
+  );
 
-    function getAllPokemons() public view returns (Pokemon[] memory) {
-      return pokemons;
-    }
+  function createPokemon (string memory _name, uint _id) public {
+    Pokemon memory pokemon = Pokemon(_id, _name);
+    pokemons.push(pokemon);
+    pokemonToOwner[_id] = msg.sender;
+    ownerPokemonCount[msg.sender]++;
+    emit eventNewPokemon(pokemon);
+  }
+
+  function getAllPokemons() public view returns (Pokemon[] memory) {
+    return pokemons;
+  }
 
 
-    function getResult() public pure returns(uint product, uint sum){
-      uint a = 1; 
-      uint b = 2;
-      product = a * b;
-      sum = a + b; 
-   }
+  function getResult() public pure returns(uint product, uint sum){
+    uint a = 1; 
+    uint b = 2;
+    product = a * b;
+    sum = a + b; 
+  }
 
 }
