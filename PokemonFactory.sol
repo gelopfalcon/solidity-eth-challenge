b5d7c14a182a8af28eeef5b70902274934f6cf29
Panchop <folivares@arenasoft.cl>
2022-07-26 00:16:05 +0100

    Reto 2

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -14,16 +14,32 @@ contract PokemonFactory {
     Pokemon newPokemon
   );
 
+  modifier idValidator(uint _id){
+    require(
+      _id > 0,
+      "The ID must be greater than 0."
+      );
+    _;
+  }
+
+  modifier nameValidator(string memory _name){
+    require(
+      bytes(_name).length > 2,
+      "The name cannot be empty and must be at least 3 characters long."
+      );
+    _;
+  }
+
   Pokemon[] private pokemons;
 
   mapping (uint => address) public pokemonToOwner;
   mapping (address => uint) ownerPokemonCount;
 
-    function createPokemon (string memory _name, uint _id) public {
-      pokemons.push(Pokemon(_id, _name));
-      pokemonToOwner[_id] = msg.sender;
-      ownerPokemonCount[msg.sender]++;
-      emit CreatePokemon(msg.sender, Pokemon(_id, _name));
+  function createPokemon (string memory _name, uint _id) public idValidator(_id) nameValidator(_name) {
+    pokemons.push(Pokemon(_id, _name));
+    pokemonToOwner[_id] = msg.sender;
+    ownerPokemonCount[msg.sender]++;
+    emit CreatePokemon(msg.sender, Pokemon(_id, _name));
   }
 
   function getAllPokemons() public view returns (Pokemon[] memory) {
