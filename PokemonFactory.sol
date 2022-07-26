a545604ab1787c71d4047028aa756b0c2279f5d2
Miguel Angel Huamán <78570710+Miguel-Huaman@users.noreply.github.com>
2022-07-26 08:50:35 -0500

    Update PokemonFactory.sol
    Edit: Ability ID, name and description

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -4,41 +4,53 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract PokemonFactory {
 
-    struct Types {
-        string name;
-        string description;
+    struct Type {
+        string TypeName;
+        string TypeDescription;
     }
 
-    struct Abilities {
-        string name;
-        string description;
-    }
-
-    struct Pokemon {
+    struct Ability {
         uint id;
         string name;
-        Ability[] abilities;
-        Type[] types;
     }
 
-    Pokemon[] private pokemons;
+    struct PokemonID {
+        uint id;
+        string name;
+    }
+
+    PokemonID[] private pokemons;
+
+    event eventNewPokemon(uint _id, string _name);
 
     mapping (uint => address) public pokemonToOwner;
     mapping (address => uint) ownerPokemonCount;
 
-    event eventNewPokemon(string _name, uint8 _id);
-
-    function createPokemon (string memory _name, uint8 _id) public {
-        pokemons.push(Pokemon(_id, _name));
+    function createPokemon (uint _id, string memory _name) public {
+        require(_id > 0, "Pokemon's ID must be greater than 0");
+        pokemons.push(PokemonID(_id, _name));
         pokemonToOwner[_id] = msg.sender;
         ownerPokemonCount[msg.sender]++;
-        emit eventNewPokemon(_name, _id);
+        emit eventNewPokemon(_id, _name);
     }
 
-    function getAllPokemons() public view returns (Pokemon[] memory) {
+        Ability[] private abilities;
+
+    event eventNewAbility(uint _id, string _name);
+
+    mapping (uint => address) public AbilityToOwner;
+    mapping (address => uint) ownerAbilityCount;
+
+    function createAbility (uint _id, string memory _name) public {
+        abilities.push(Ability(_id, _name));
+        AbilityToOwner[_id] = msg.sender;
+        ownerAbilityCount[msg.sender]++;
+        emit eventNewAbility(_id, _name);
+    }
+
+    function getAllPokemons() public view returns (PokemonID[] memory) {
       return pokemons;
     }
-
 
     function getResult() public pure returns(uint product, uint sum){
       uint a = 1; 
