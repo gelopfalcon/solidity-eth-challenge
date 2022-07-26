5b7e59f4feaf2f97b573bda6bd91ab9064b7c19b
Miguel Angel Huamán <78570710+Miguel-Huaman@users.noreply.github.com>
2022-07-26 09:35:26 -0500

    New: Retos resueltos

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -5,13 +5,15 @@ pragma solidity >=0.7.0 <0.9.0;
 contract PokemonFactory {
 
     struct Type {
-        string TypeName;
-        string TypeDescription;
+        uint id;
+        string name;
+        string description;
     }
 
     struct Ability {
         uint id;
         string name;
+        string description;
     }
 
     struct PokemonID {
@@ -34,22 +36,52 @@ contract PokemonFactory {
         emit eventNewPokemon(_id, _name);
     }
 
-        Ability[] private abilities;
+    function getAllPokemons() public view returns (PokemonID[] memory) {
+      return pokemons;
+    }
 
-    event eventNewAbility(uint _id, string _name);
+
+    Ability[] private abilities;
+
+    event eventNewAbility(uint _id, string _name, string _description);
 
     mapping (uint => address) public AbilityToOwner;
     mapping (address => uint) ownerAbilityCount;
 
-    function createAbility (uint _id, string memory _name) public {
-        abilities.push(Ability(_id, _name));
+    function createAbility (uint _id, string memory _name, string memory _description) public {
+        require(_id > 0, "Ability's ID must be greater than 0");
+        require(bytes(_name).length > 2, "Ability's Name must be longer than 2 characters");
+        require(bytes(_description).length > 2, "Ability's Description must be longer than 2 characters");
+        abilities.push(Ability(_id, _name, _description));
         AbilityToOwner[_id] = msg.sender;
         ownerAbilityCount[msg.sender]++;
-        emit eventNewAbility(_id, _name);
+        emit eventNewAbility(_id, _name, _description);
     }
 
-    function getAllPokemons() public view returns (PokemonID[] memory) {
-      return pokemons;
+    function getAllAbilities() public view returns (Ability[] memory) {
+      return abilities;
+    }
+
+
+    Type[] private types;
+
+    event eventNewType(uint _id, string _name, string _description);
+
+    mapping (uint => address) public TypeToOwner;
+    mapping (address => uint) ownerTypeCount;
+
+    function createType (uint _id, string memory _name, string memory _description) public {
+        require(_id > 0, "Type's ID must be greater than 0");
+        require(bytes(_name).length > 2, "Types's Name must be longer than 2 characters");
+        require(bytes(_description).length > 2, "Types's Description must be longer than 2 characters");
+        types.push(Type(_id, _name, _description));
+        TypeToOwner[_id] = msg.sender;
+        ownerTypeCount[msg.sender]++;
+        emit eventNewType(_id, _name, _description);
+    }
+
+    function getAllTypes() public view returns (Type[] memory) {
+      return types;
     }
 
     function getResult() public pure returns(uint product, uint sum){
