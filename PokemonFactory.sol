ca08f84376e9a0c732dcb4111fead28f1990b6fe
Candela Bramucci <candelabramucci05@gmail.com>
2022-07-26 16:37:19 -0300

    Reto 1: Añadiendo evento eventNewPokemon

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -13,10 +13,13 @@ contract PokemonFactory {
     mapping(uint256 => address) public pokemonToOwner;
     mapping(address => uint256) ownerPokemonCount;
 
+    event eventNewPokemon(string pokemonName, uint256 pokemonId);
+
     function createPokemon(string memory _name, uint256 _id) public {
         pokemons.push(Pokemon(_id, _name));
         pokemonToOwner[_id] = msg.sender;
         ownerPokemonCount[msg.sender]++;
+        emit eventNewPokemon(_name, _id);
     }
 
     function getAllPokemons() public view returns (Pokemon[] memory) {
