b04009f8bb7dd6c71cce848a1e2fa0d5c5a79187
Sergio Valadez <sergioval@gmail.com>
2022-07-26 11:28:43 -0500

    Reto 1 y 2

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -3,33 +3,29 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract PokemonFactory {
+    event createPokemonEvent(uint8 id, string name);
 
-  struct Pokemon {
-    uint id;
-    string name;
-  }
+    struct Pokemon {
+        uint8 id;
+        string name;
+    }
 
     Pokemon[] private pokemons;
 
-    mapping (uint => address) public pokemonToOwner;
-    mapping (address => uint) ownerPokemonCount;
+    mapping (uint8 => address) public pokemonToOwner;
+    mapping (address => uint8) ownerPokemonCount;
 
-     function createPokemon (string memory _name, uint _id) public {
+     function createPokemon (string memory _name, uint8 _id) public {
+        require (_id > 0, "EL id no puede ser menor que 1");
+        require (bytes(_name).length > 2, "el nombre no debe ir vacio");
         pokemons.push(Pokemon(_id, _name));
         pokemonToOwner[_id] = msg.sender;
         ownerPokemonCount[msg.sender]++;
+        emit createPokemonEvent(_id, _name);
     }
 
     function getAllPokemons() public view returns (Pokemon[] memory) {
       return pokemons;
     }
-
-
-    function getResult() public pure returns(uint product, uint sum){
-      uint a = 1; 
-      uint b = 2;
-      product = a * b;
-      sum = a + b; 
-   }
 
 }
