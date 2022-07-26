9ee80f1c68c9f521dd48a8e101992e4d46d57a36
Luis Fernando Ushiña <guffenix@gmail.com>
2022-07-26 12:19:23 -0500

    implementation of eventNewPokemon event

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -3,33 +3,35 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract PokemonFactory {
-
-  struct Pokemon {
-    uint id;
-    string name;
-  }
+    struct Pokemon {
+        uint256 id;
+        string name;
+    }
 
     Pokemon[] private pokemons;
 
-    mapping (uint => address) public pokemonToOwner;
-    mapping (address => uint) ownerPokemonCount;
+    mapping(uint256 => address) public pokemonToOwner;
+    mapping(address => uint256) ownerPokemonCount;
 
-     function createPokemon (string memory _name, uint _id) public {
-        pokemons.push(Pokemon(_id, _name));
+    event eventNewPokemon(Pokemon _pokemonCreated);
+
+    function createPokemon(string memory _name, uint256 _id) public {
+        Pokemon memory pokemonCreated = Pokemon(_id, _name);
+        pokemons.push(pokemonCreated);
         pokemonToOwner[_id] = msg.sender;
         ownerPokemonCount[msg.sender]++;
+
+        emit eventNewPokemon(pokemonCreated);
     }
 
     function getAllPokemons() public view returns (Pokemon[] memory) {
-      return pokemons;
+        return pokemons;
     }
 
-
-    function getResult() public pure returns(uint product, uint sum){
-      uint a = 1; 
-      uint b = 2;
-      product = a * b;
-      sum = a + b; 
-   }
-
+    function getResult() public pure returns (uint256 product, uint256 sum) {
+        uint256 a = 1;
+        uint256 b = 2;
+        product = a * b;
+        sum = a + b;
+    }
 }
