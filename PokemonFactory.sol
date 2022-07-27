903b3b80c4952ff275d781d7c87d20e96af15ab9
Panchop <folivares@arenasoft.cl>
2022-07-27 20:50:36 +0100

    Reto 3

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -9,6 +9,11 @@ contract PokemonFactory {
     string name;
   }
 
+  struct Ability {
+    string name;
+    string description;
+  }
+
   event CreatePokemon(
     address creator,
     Pokemon newPokemon
@@ -34,21 +39,31 @@ contract PokemonFactory {
 
   mapping (uint => address) public pokemonToOwner;
   mapping (address => uint) ownerPokemonCount;
+  mapping (uint => Ability[]) abilities;
 
   function createPokemon (string memory _name, uint _id) public idValidator(_id) nameValidator(_name) {
+    // create pokemon, asign to owner, and emit an event
     pokemons.push(Pokemon(_id, _name));
     pokemonToOwner[_id] = msg.sender;
     ownerPokemonCount[msg.sender]++;
     emit CreatePokemon(msg.sender, Pokemon(_id, _name));
   }
 
+  function addAbility (uint _id, string memory _name, string memory _description) public idValidator(_id) nameValidator(_name) {
+    // add ability to pokemon ability list
+    abilities[_id].push(Ability(_name, _description));
+  }
+
   function getAllPokemons() public view returns (Pokemon[] memory) {
     return pokemons;
   }
 
+  function getAbility(uint _id) public view idValidator(_id) returns (Ability[] memory) {
+    return abilities[_id];
+  }
 
   function getResult() public pure returns(uint product, uint sum){
-    uint a = 1; 
+    uint a = 1;
     uint b = 2;
     product = a * b;
     sum = a + b; 
