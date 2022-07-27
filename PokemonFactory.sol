4a567bda945a86dab4cfdf45a9cf5f9c66869a3b
Jaime Jiménez <jimenez.ing.sis@gmail.com>
2022-07-27 13:17:57 -0500

    Challenge #4
    Challenge #4 - Pokemon Weaknesses and types

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -14,6 +14,7 @@ contract PokemonFactory {
         string name;
     }
     
+    enum Type { NORMAL, WATER, GRASS, FIRE, ELECTRIC, ICE, FIGHTING, PISON, GROUND, FLYING, PSYCHIC, BUG, ROCK, GHOST, DARK, DRAGON, STEEL, FAIRY }
 
     event eventNewPokemon(Pokemon pokemon);
 
@@ -22,15 +23,30 @@ contract PokemonFactory {
     mapping (uint => Skill[]) public pokemonToSkills;
     mapping (uint => address) public pokemonToOwner;
     mapping (address => uint) ownerPokemonCount;
+    mapping (uint => Type[]) pokemonToTypes;
+    mapping (uint => Type[]) pokemonToWeaknesses;
 
-    function createPokemon (string memory _name, uint _id, Skill[] memory _skills) public {
+
+    function createPokemon (string memory _name, uint _id, Skill[] memory _skills, Type[] memory _types, Type[] memory _weaknesses) public {
         require(_id > 0, "Id should be more than 0.");
         require(bytes(_name).length >= 2, "Name needs at least two letters");
-        uint256 length = _skills.length;
-        for (uint256 i = 0; i < length; i+=1) {
+        pokemons.push(Pokemon(_id, _name));
+        // loop skills to push in storage variable
+        uint256 skillsLength = _skills.length;
+        for (uint i = 0; i < skillsLength; i+=1) {
             pokemonToSkills[_id].push(Skill(_skills[i].name,_skills[i].description));
         }
-        pokemons.push(Pokemon(_id, _name));
+        //loop types to push in storage variable
+        uint256 typesLength = _types.length;
+        for (uint j = 0; j < typesLength; j+=1) {
+            pokemonToTypes[_id].push(_types[j]);
+        }
+        //loop types to push in storage variable
+        uint256 weaknessesLength = _weaknesses.length;
+        for (uint k = 0; k < weaknessesLength; k+=1) {
+            pokemonToWeaknesses[_id].push(_weaknesses[k]);
+        }
+
         pokemonToOwner[_id] = msg.sender;
         ownerPokemonCount[msg.sender]++;
         emit eventNewPokemon(Pokemon(_id, _name));
@@ -43,6 +59,14 @@ contract PokemonFactory {
 
     function getPokemonSkills(uint256 _id) public view returns (Skill[] memory) {
       return pokemonToSkills[_id];
+    }
+
+    function getPokemonTypes(uint256 _id) public view returns (Type[] memory) {
+      return pokemonToTypes[_id];
+    }
+
+    function getPokemonWeaknesses(uint256 _id) public view returns (Type[] memory) {
+      return pokemonToWeaknesses[_id];
     }
 
     function getResult() public pure returns(uint product, uint sum, uint d){
