d4aae7e4710afe1213418f95e2fbce830255c874
nilt0n <info@nilton.org>
2022-07-29 22:30:08 -0500

    pokemon funcion

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -39,25 +39,71 @@ contract PokemonFactory {
 
     Pokemon[] private pokemons;
 
-    mapping (uint => address) public pokemonToOwner;
-    mapping (address => uint) ownerPokemonCount;
+    mapping(uint256 => address) public pokemonToOwner;
+    mapping(address => uint256) ownerPokemonCount;
 
-     function createPokemon (string memory _name, uint _id) public {
-        pokemons.push(Pokemon(_id, _name));
+    event eventNewPokemon(address owner, uint256 id, string name);
+
+    modifier correctPokemonData(
+        string memory _name,
+        uint256 _id,
+        Element[] memory types,
+        Element[] memory weaknesses
+    ) {
+        // We need to verify if the id is greater than 0
+        require(_id > 0, "The id must be greater than 0");
+        // We need to verify if name is not empty
+        require(
+            bytes(_name).length > 2,
+            "The name cannot be empty and must be longer than two characters"
+        );
+        // We need to verify if the types length is less or equal than 2
+        require(types.length <= 2, "A pokemon can only have two types maximum");
+        // We need to verify if the weaknesses length is less or equal than 5
+        require(
+            weaknesses.length <= 5,
+            "A pokemon can only have two weaknesses maximum"
+        );
+        // We need to check if weaknesses and types are different
+        for (uint256 i = 0; i < weaknesses.length; i++) {
+            for (uint256 j = 0; j < types.length; j++) {
+                require(
+                    weaknesses[i] != types[j],
+                    "Weaknesses and types must be different"
+                );
+            }
+        }
+        // The function is inserted where this symbol appears
+        _;
+    }
+
+    function createPokemon(
+        string memory _name,
+        uint256 _id,
+        Ability[] memory abilities,
+        Element[] memory types,
+        Element[] memory weaknesses
+    ) public correctPokemonData(_name, _id, types, weaknesses) {
+        pokemons.push();
+        uint256 index = pokemons.length - 1;
+        pokemons[index].id = _id;
+        pokemons[index].name = _name;
+        uint256 i = 0;
+        for (i = 0; i < abilities.length; i++) {
+            pokemons[index].abilities.push(abilities[i]);
+        }
+        for (i = 0; i < types.length; i++) {
+            pokemons[index].types.push(types[i]);
+        }
+        for (i = 0; i < weaknesses.length; i++) {
+            pokemons[index].weaknesses.push(weaknesses[i]);
+        }
         pokemonToOwner[_id] = msg.sender;
         ownerPokemonCount[msg.sender]++;
+        emit eventNewPokemon(msg.sender, _id, _name);
     }
 
     function getAllPokemons() public view returns (Pokemon[] memory) {
-      return pokemons;
+        return pokemons;
     }
-
-
-    function getResult() public pure returns(uint product, uint sum){
-      uint a = 1; 
-      uint b = 2;
-      product = a * b;
-      sum = a + b; 
-   }
-
 }
