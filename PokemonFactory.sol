307e20f18433f55ca59fe685217648ac78c63094
Gerardo Miranda <gerardodev25@gmail.com>
2022-07-29 20:00:23 -0400

    working in the reto 4

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -11,11 +11,18 @@ contract PokemonFactory {
         string description;
     }
 
+    struct Type {
+        uint id;
+        string name;
+    }
+
     struct Pokemon {
         uint id;
         string name;
         string urlImg;
         Ability[] abilities;
+        Type[] pokemonTypes;
+        Type[] weaknessesTypes;
     }
 
     Pokemon[] private pokemons;
@@ -39,7 +46,9 @@ contract PokemonFactory {
         string memory _name,
         string memory _urlImg,
         string[] memory _skillNames,
-        string[] memory _skillDescriptions
+        string[] memory _skillDescriptions,
+        string[] memory _pokemonTypes,
+        string[] memory _weaknessesTypes
         ) public onlyOwner{
 
         require(_id > 0, "the id shout be major that 0");
@@ -53,6 +62,8 @@ contract PokemonFactory {
         pokemons[index].name = _urlImg;
 
         addAbilities(_skillNames, _skillDescriptions, index);
+        addpokemonTypes(_pokemonTypes, index);
+        addweaknessesTypes(_weaknessesTypes, index);
 
         pokemonToOwner[_id] = msg.sender;
         ownerPokemonCount[msg.sender]++;
@@ -64,6 +75,18 @@ contract PokemonFactory {
     function addAbilities(string[] memory _skillNames, string[] memory _skillDescriptions, uint index) private {
         for(uint i; i < _skillNames.length; i++){
             pokemons[index].abilities.push(Ability(_skillNames[i], _skillDescriptions[i]));
+        }
+    }
+
+    function addpokemonTypes(string[] memory _pokemonTypes, uint index) private{
+        for(uint i; i< _pokemonTypes.length; i++){
+            pokemons[index].pokemonTypes.push(Type(i, _pokemonTypes[i] ));
+        }
+    }
+
+    function addweaknessesTypes(string[] memory _weaknessesTypes, uint index) private{
+        for(uint i; i< _weaknessesTypes.length; i++){
+            pokemons[index].weaknessesTypes.push(Type(i, _weaknessesTypes[i] ));
         }
     }
 
