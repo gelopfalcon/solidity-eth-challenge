7c6687af4eed6b82cdb26aa59b0daf0028aab1a7
Nadia <enadialopez@gmail.com>
2022-07-31 20:08:23 -0300

    refactory

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -14,28 +14,39 @@ contract PokemonFactory {
         string description;
     }
 
-    Pokemon[] private pokemons;
+  Pokemon[] private pokemons;
 
-    mapping (uint => address) public pokemonToOwner;
-    mapping (address => uint) ownerPokemonCount;
-    mapping (uint8 => Hability []) public habilities;
+  mapping (uint => address) public pokemonToOwner;
+  mapping (address => uint) ownerPokemonCount;
+  mapping (uint8 => Hability []) public habilities;
 
-    event eventNewPokemon (address indexed_from, uint8 indexed_to);
+  event eventNewPokemon (address indexed_from, uint8 indexed_to);
 
 
-     function createPokemon (string memory _name, uint _id) public {
+  function createPokemon (string memory _name, uint8 _id) public {
         require( bytes(_name).length != 0, "Name can not be empty");
         require(_id > 0, "ID does not have to be less o equal than 0 ");
         Pokemon memory pokemon = Pokemon(_id, _name);
         pokemons.push(pokemon);
         pokemonToOwner[_id] = msg.sender;
         ownerPokemonCount[msg.sender]++;
-        habilities[pokemon.id].push(Hability(name_hability, description_hability));
-        emit eventNewPokemon (pokemonToOwner [_id], _id);
+        emit eventNewPokemon (msg.sender, _id, _name);
     }
 
     function getAllPokemons() public view returns (Pokemon[] memory) {
-      return pokemons;
+        return pokemons;
+    }
+
+    function addHability (uint8 _id, string memory _name, string memory _description )public {
+        habilities[_id].push(Hability(_name, _description));
+    }
+
+    function addType (uint8 _id, string memory _type )public {
+        types[_id].push(_type);
+    }
+
+    function addWeakness(uint8 _id, string memory _weakness)public {
+        weaknesses[_id].push(_weakness);
     }
 
 
