8bd7a81c43bf9aadd3bb25b9b2cc314c284f708e
IvySaskia <ivysaskia@gmail.com>
2022-08-02 10:11:39 -0400

    [gelopfalcon/solidity-eth-challenge#3] Add: Reto 3 version 2

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -22,19 +22,26 @@ contract PokemonFactory {
 
     event eventNewPokemon (Pokemon pokemon);
 
-    function createPokemon (string memory _name, uint _id, Ability[] memory _abilities) public {
-      require(_id > 0, "Pokemon's id should be greater than 0");     
-      require(bytes(_name).length > 2, "Pokemon's name should have more that two characters.");
-      Pokemon storage pokemon = pokemons.push();
-      pokemon.id = _id;
-      pokemon.name = _name;
-      for (uint i = 0; i < _abilities.length; i++) {
-          pokemon.abilities.push(_abilities[i]);
+    function createPokemon (string memory _name, uint _id) public {
+        require(_id > 0, "Pokemon's id should be greater than 0");     
+        require(bytes(_name).length > 2, "Pokemon's name should have more that two characters.");
+        Pokemon storage pokemon = pokemons.push();
+        pokemon.id = _id;
+        pokemon.name = _name;        
+        pokemonToOwner[_id] = msg.sender;
+        ownerPokemonCount[msg.sender]++;
+        emit eventNewPokemon (pokemon);
+    }
+
+    function addAbilitiesToPokemon(uint _idPokemon, Ability[] memory _abilities) public {
+      for (uint256 i; i<pokemons.length; i++) {
+        if (pokemons[i].id == _idPokemon) {
+            for (uint j = 0; j < _abilities.length; j++) {
+              pokemons[i].abilities.push(_abilities[j]);
+            }
+            break;
+        } 
       }
-      
-      pokemonToOwner[_id] = msg.sender;
-      ownerPokemonCount[msg.sender]++;
-      emit eventNewPokemon (pokemon);
     }
 
     function getAllPokemons() public view returns (Pokemon[] memory) {
