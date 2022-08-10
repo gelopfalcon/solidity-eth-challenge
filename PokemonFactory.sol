db9a471aebf5f190483174624703200fc1cd1bdd
Matias Leiva <matiasoscarleiva@gmail.com>
2022-08-10 16:51:20 -0300

    completa reto #3

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -4,12 +4,19 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract PokemonFactory {
 
+  struct Ability {
+    string name;
+    string description;
+  }
+
   struct Pokemon {
     uint id;
     string name;
+    Ability[] abilities;
   }
 
   Pokemon[] private pokemons;
+  Ability[] private abilities;
 
   mapping (uint => address) public pokemonToOwner;
   mapping (address => uint) ownerPokemonCount;
@@ -19,25 +26,27 @@ contract PokemonFactory {
     string name
   );
 
-    function createPokemon (string memory _name, uint _id) public {
-      require(_id > 0, "El Id debe ser mayor a 0");
-      require(bytes(_name).length != 0, "El nombre no puede estar vacio");
-      require(bytes(_name).length > 2, "El nombre debe ser mayor a 2 caracteres");
-      pokemons.push(Pokemon(_id, _name));
-      pokemonToOwner[_id] = msg.sender;
-      ownerPokemonCount[msg.sender]++;
-      emit eventNewPokemon(_id, _name);
+  function createPokemon (string memory _name, uint _id, uint[] memory _abilities) public {
+    require(_id > 0, "El Id debe ser mayor a 0");
+    require(bytes(_name).length != 0, "El nombre no puede estar vacio");
+    require(bytes(_name).length > 2, "El nombre debe ser mayor a 2 caracteres");
+    Pokemon storage p = pokemons.push();
+    for(uint i = 0; i < _abilities.length; i++){
+      p.abilities.push(abilities[i]);
+    } 
+    p.id = _id;
+    p.name = _name;
+    pokemonToOwner[_id] = msg.sender;
+    ownerPokemonCount[msg.sender]++;
+    emit eventNewPokemon(_id, _name);
+  }
+
+  function createAbility (string memory _name, string memory _desc) public {
+    abilities.push(Ability(_name, _desc));
   }
 
   function getAllPokemons() public view returns (Pokemon[] memory) {
     return pokemons;
-  }
-
-  function getResult() public pure returns(uint product, uint sum){
-    uint a = 1; 
-    uint b = 2;
-    product = a * b;
-    sum = a + b; 
   }
 
 }
