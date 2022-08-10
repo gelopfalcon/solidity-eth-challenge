b0eec354e67bf889d957891cf9882cc44500bb55
Matias Leiva <matiasoscarleiva@gmail.com>
2022-08-10 15:47:12 -0300

    completa reto #2

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -9,33 +9,35 @@ contract PokemonFactory {
     string name;
   }
 
-    Pokemon[] private pokemons;
+  Pokemon[] private pokemons;
 
-    mapping (uint => address) public pokemonToOwner;
-    mapping (address => uint) ownerPokemonCount;
+  mapping (uint => address) public pokemonToOwner;
+  mapping (address => uint) ownerPokemonCount;
 
-    event eventNewPokemon(
-      uint id,
-      string name
-    );
+  event eventNewPokemon(
+    uint id,
+    string name
+  );
 
-     function createPokemon (string memory _name, uint _id) public {
-        pokemons.push(Pokemon(_id, _name));
-        pokemonToOwner[_id] = msg.sender;
-        ownerPokemonCount[msg.sender]++;
-        emit eventNewPokemon(_id, _name);
-    }
+    function createPokemon (string memory _name, uint _id) public {
+      require(_id > 0, "El Id debe ser mayor a 0");
+      require(bytes(_name).length != 0, "El nombre no puede estar vacio");
+      require(bytes(_name).length > 2, "El nombre debe ser mayor a 2 caracteres");
+      pokemons.push(Pokemon(_id, _name));
+      pokemonToOwner[_id] = msg.sender;
+      ownerPokemonCount[msg.sender]++;
+      emit eventNewPokemon(_id, _name);
+  }
 
-    function getAllPokemons() public view returns (Pokemon[] memory) {
-      return pokemons;
-    }
+  function getAllPokemons() public view returns (Pokemon[] memory) {
+    return pokemons;
+  }
 
-
-    function getResult() public pure returns(uint product, uint sum){
-      uint a = 1; 
-      uint b = 2;
-      product = a * b;
-      sum = a + b; 
-   }
+  function getResult() public pure returns(uint product, uint sum){
+    uint a = 1; 
+    uint b = 2;
+    product = a * b;
+    sum = a + b; 
+  }
 
 }
