63bc4ed3ab71b2bebe02773eb955a169e49f6abd
Matias Leiva <matiasoscarleiva@gmail.com>
2022-08-10 18:16:37 -0300

    completa reto #4

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -13,7 +13,11 @@ contract PokemonFactory {
     uint id;
     string name;
     Ability[] abilities;
+    string[] types;
+    string[] debilities;
   }
+
+  string[] public Types = ['Normal', 'Fire', 'Water', 'Grass', 'Flying', 'Fighting', 'Poison', 'Electric', 'Ground', 'Rock', 'Psychic', 'Ice', 'Bug', 'Ghost', 'Steel', 'Dragon', 'Dark', 'Fairy'];
 
   Pokemon[] private pokemons;
   Ability[] private abilities;
@@ -26,14 +30,20 @@ contract PokemonFactory {
     string name
   );
 
-  function createPokemon (string memory _name, uint _id, uint[] memory _abilities) public {
+  function createPokemon (string memory _name, uint _id, uint8[] memory _abilities, uint8[] memory _types, uint8[] memory _debilities) public {
     require(_id > 0, "El Id debe ser mayor a 0");
     require(bytes(_name).length != 0, "El nombre no puede estar vacio");
     require(bytes(_name).length > 2, "El nombre debe ser mayor a 2 caracteres");
     Pokemon storage p = pokemons.push();
     for(uint i = 0; i < _abilities.length; i++){
       p.abilities.push(abilities[i]);
-    } 
+    }
+    for(uint i = 0; i < _types.length; i++){
+      p.types.push(Types[_types[i]]);
+    }
+    for(uint i = 0; i < _debilities.length; i++){
+      p.debilities.push(Types[_debilities[i]]);
+    }
     p.id = _id;
     p.name = _name;
     pokemonToOwner[_id] = msg.sender;
