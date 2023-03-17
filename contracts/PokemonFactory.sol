3aa6ecfffb88daa63570e80276b34e94ab60b1fc
ArteInverso <info@arteinverso.co>
2023-03-16 20:26:54 -0500

    require + evolution

--- contracts/PokemonFactory.sol
+++ contracts/PokemonFactory.sol
@@ -9,26 +9,48 @@ contract PokemonFactory {
     string name;
   }
 
+  struct Habilities {
+    string Name;
+    string Description;
+  }
+
   event eventNewPokemon(
     uint id, 
     string name
   );
 
+  modifier onlyId (uint _id) {
+    require( _id > 0,
+    "El id de tu pokemon debe ser mayor a 0");
+    _;
+  }
+
+  modifier onlyName (string memory _name) {
+    require( bytes(_name).length > 2,
+    "El nombre de tu pokemon debe ser mayor a 2 caracteres");
+    _;
+  }
 
     Pokemon[] private pokemons;
+    Habilities[] private habilities;
 
     mapping (uint => address) public pokemonToOwner;
     mapping (address => uint) ownerPokemonCount;
 
-     function createPokemon (string memory _name, uint _id) public {
+     function createPokemon (string memory _name, uint _id) public onlyId(_id) onlyName(_name) {
         pokemons.push(Pokemon(_id, _name));
         pokemonToOwner[_id] = msg.sender;
         ownerPokemonCount[msg.sender]++;
         emit eventNewPokemon(_id, _name);
     }
 
-    function getAllPokemons() public view returns (Pokemon[] memory) {
-      return pokemons;
+    function evolutionPokemon (string memory _Name, string memory _Description, uint _id) public {
+        habilities.push(Habilities(_Name, _Description));
+        pokemonToOwner[_id] = msg.sender;
+    }
+
+    function getAllPokemons() public view returns (Pokemon[] memory, Habilities[] memory) {
+        return (pokemons, habilities);
     }
 
 
